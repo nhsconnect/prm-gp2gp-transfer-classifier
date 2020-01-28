89d8a92a38f839644f235727215c072363ddc8c2
aiwatko <oktawia.kata@thoughtworks.com>
2020-01-28 16:00:15 +0000

    PRMT-791 [OK/DS/JG] Introduce custom assertion for practice sla summary ods codes

--- tests/unit/gp2gp/transformers/test_calculate_sla_by_practice.py
+++ tests/unit/gp2gp/transformers/test_calculate_sla_by_practice.py
@@ -1,17 +1,24 @@
+from collections import Counter
 from datetime import timedelta
+from typing import Set, Iterable
 
+from prmdata.gp2gp.models import PracticeSlaSummary
 from prmdata.gp2gp.transformers import calculate_sla_by_practice
 from tests.builders.gp2gp import build_transfer
 
 
+def _assert_has_ods_codes(practices: Iterable[PracticeSlaSummary], expected: Set[str]):
+    actual_counts = Counter((practice.ods for practice in practices))
+    expected_counts = Counter(expected)
+    assert actual_counts == expected_counts
+
+
 def test_calculate_sla_by_practice_given_single_transfer():
-    transfer = build_transfer(requesting_practice_ods="A12345")
+    transfers = [build_transfer(requesting_practice_ods="A12345")]
 
-    actual = calculate_sla_by_practice([transfer])
-    actual_ods = [practice.ods for practice in actual]
-    expected = ["A12345"]
+    actual = calculate_sla_by_practice(transfers)
 
-    assert actual_ods == expected
+    _assert_has_ods_codes(actual, {"A12345"})
 
 
 def test_calculate_sla_by_practice_returns_correct_practice_sla_summary_for_one_transfer():
