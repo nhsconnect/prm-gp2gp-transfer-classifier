94010d2dc1e5a0aaaf45da8688cdd6ad609b9a5f
James Green <jagreen@thoughtworks.com>
2020-02-03 10:33:04 +0000

    PRMT-791 [JG/OK/DS] Add 'generated on' timestamp to ServiceDashboardData

--- src/gp2gp/dashboard/transformers.py
+++ src/gp2gp/dashboard/transformers.py
@@ -4,4 +4,4 @@ from gp2gp.dashboard.models import ServiceDashboardData
 
 
 def construct_service_dashboard_data() -> ServiceDashboardData:
-    return ServiceDashboardData(generated_on=datetime(year=2019, month=6, day=2, hour=23, second=42))
+    return ServiceDashboardData(generated_on=datetime.now())
--- tests/unit/dashboard/transformers/test_construct_service_dashboard_data.py
+++ tests/unit/dashboard/transformers/test_construct_service_dashboard_data.py
@@ -5,8 +5,17 @@ from gp2gp.dashboard.transformers import construct_service_dashboard_data
 
 
 @freeze_time(datetime(year=2019, month=6, day=2, hour=23, second=42))
-def test_dashboard_data_has_correct_generated_on_timestamp():
+def test_dashboard_data_has_correct_generated_on_given_time():
     expected = datetime(year=2019, month=6, day=2, hour=23, second=42)
+
+    actual = construct_service_dashboard_data()
+
+    assert actual.generated_on == expected
+
+
+@freeze_time(datetime(year=2019, month=4, day=28, hour=23, second=42))
+def test_dashboard_data_has_correct_generated_on_given_different_time():
+    expected = datetime(year=2019, month=4, day=28, hour=23, second=42)
 
     actual = construct_service_dashboard_data()
 
