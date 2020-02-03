6ddd5855f11e342e68ba834f60e2703c0548cca7
James Green <jagreen@thoughtworks.com>
2020-02-03 12:05:35 +0000

    PRMT-791 [OK/DS/JG] Be more consistent with test variable naming

--- tests/unit/dashboard/transformers/test_construct_service_dashboard_data.py
+++ tests/unit/dashboard/transformers/test_construct_service_dashboard_data.py
@@ -17,20 +17,20 @@ def _assert_has_ods_codes(practices: Iterable[PracticeSummary], expected: Set[st
 
 @freeze_time(datetime(year=2019, month=6, day=2, hour=23, second=42))
 def test_dashboard_data_has_correct_generated_on_given_time():
-    expected = datetime(year=2019, month=6, day=2, hour=23, second=42)
+    expected_generated_on = datetime(year=2019, month=6, day=2, hour=23, second=42)
 
     actual = construct_service_dashboard_data(sla_metrics=[])
 
-    assert actual.generated_on == expected
+    assert actual.generated_on == expected_generated_on
 
 
 @freeze_time(datetime(year=2019, month=4, day=28, hour=23, second=42))
 def test_dashboard_data_has_correct_generated_on_given_different_time():
-    expected = datetime(year=2019, month=4, day=28, hour=23, second=42)
+    expected_generated_on = datetime(year=2019, month=4, day=28, hour=23, second=42)
 
     actual = construct_service_dashboard_data(sla_metrics=[])
 
-    assert actual.generated_on == expected
+    assert actual.generated_on == expected_generated_on
 
 
 def test_dashboard_data_has_correct_ods_given_a_single_practice():
--- tests/unit/service/transformers/test_derive_transfer.py
+++ tests/unit/service/transformers/test_derive_transfer.py
@@ -6,11 +6,12 @@ from tests.builders.spine import build_parsed_conversation, build_message
 
 def test_derive_transfer_extracts_conversation_id():
     conversation = build_parsed_conversation(id="1234")
-    transfer = derive_transfer(conversation)
 
-    expected = "1234"
-    actual = transfer.conversation_id
-    assert actual == expected
+    actual = derive_transfer(conversation)
+
+    expected_conversation_id = "1234"
+
+    assert actual.conversation_id == expected_conversation_id
 
 
 def test_derive_transfer_produces_sla_of_successful_conversation():
@@ -23,22 +24,22 @@ def test_derive_transfer_produces_sla_of_successful_conversation():
         ),
     )
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = timedelta(hours=1, minutes=10)
-    actual = transfer.sla_duration
-    assert actual == expected
+    expected_sla_duration = timedelta(hours=1, minutes=10)
+
+    assert actual.sla_duration == expected_sla_duration
 
 
 def test_derive_transfer_produces_no_sla_given_pending_ehr_completed():
     conversation = build_parsed_conversation(
         request_started=build_message(), request_completed=None, request_completed_ack=None,
     )
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = None
-    actual = transfer.sla_duration
-    assert actual == expected
+    expected_sla_duration = None
+
+    assert actual.sla_duration == expected_sla_duration
 
 
 def test_derive_transfer_produces_no_sla_given_pending_request_completed_ack():
@@ -47,51 +48,50 @@ def test_derive_transfer_produces_no_sla_given_pending_request_completed_ack():
         request_completed=build_message(),
         request_completed_ack=None,
     )
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = None
-    actual = transfer.sla_duration
-    assert actual == expected
+    expected_sla_duration = None
+
+    assert actual.sla_duration == expected_sla_duration
 
 
 def test_derive_transfer_extracts_requesting_practice_ods():
     conversation = build_parsed_conversation(request_started=build_message(from_party_ods="A12345"))
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = "A12345"
-    actual = transfer.requesting_practice_ods
-    assert actual == expected
+    expected_ods = "A12345"
+    assert actual.requesting_practice_ods == expected_ods
 
 
 def test_derive_transfer_extracts_sending_practice_ods():
     conversation = build_parsed_conversation(request_started=build_message(to_party_ods="A12377"))
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = "A12377"
-    actual = transfer.sending_practice_ods
-    assert actual == expected
+    expected_ods = "A12377"
+
+    assert actual.sending_practice_ods == expected_ods
 
 
 def test_derive_transfer_extracts_error_code():
     conversation = build_parsed_conversation(request_completed_ack=build_message(error_code=99))
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = 99
-    actual = transfer.error_code
-    assert actual == expected
+    expected_error = 99
+
+    assert actual.error_code == expected_error
 
 
 def test_derive_transfer_doesnt_extract_error_code_given_pending_request_completed_ack():
     conversation = build_parsed_conversation(request_completed_ack=None)
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = None
-    actual = transfer.error_code
-    assert actual == expected
+    expected_error = None
+
+    assert actual.error_code == expected_error
 
 
 def test_derive_transfer_flags_pending_request_completed_as_pending():
@@ -99,11 +99,11 @@ def test_derive_transfer_flags_pending_request_completed_as_pending():
         request_started=build_message(), request_completed=None, request_completed_ack=None
     )
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = True
-    actual = transfer.pending
-    assert actual == expected
+    expected_pending = True
+
+    assert actual.pending == expected_pending
 
 
 def test_derive_transfer_flags_pending_request_completed_ack_as_pending():
@@ -113,11 +113,11 @@ def test_derive_transfer_flags_pending_request_completed_ack_as_pending():
         request_completed_ack=None,
     )
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = True
-    actual = transfer.pending
-    assert actual == expected
+    expected_pending = True
+
+    assert actual.pending == expected_pending
 
 
 def test_derive_transfer_flags_completed_conversation_as_not_pending():
@@ -127,8 +127,8 @@ def test_derive_transfer_flags_completed_conversation_as_not_pending():
         request_completed_ack=build_message(),
     )
 
-    transfer = derive_transfer(conversation)
+    actual = derive_transfer(conversation)
 
-    expected = False
-    actual = transfer.pending
-    assert actual == expected
+    expected_pending = False
+
+    assert actual.pending == expected_pending
