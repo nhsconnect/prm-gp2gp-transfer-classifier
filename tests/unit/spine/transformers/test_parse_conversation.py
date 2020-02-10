475f9abc562a3a080c1cfd2a753434a9dee4ca22
James Green <jagreen@thoughtworks.com>
2020-02-10 17:52:37 +0000

    PRMT-791 Simplify guids in parser unit tests

--- tests/unit/spine/transformers/test_parse_conversation.py
+++ tests/unit/spine/transformers/test_parse_conversation.py
@@ -11,12 +11,10 @@ from tests.builders.spine import build_message
 
 def test_parse_conversation_parses_a_complete_conversation():
     request_started_message = build_message(interaction_id=EHR_REQUEST_STARTED)
-    request_completed_message = build_message(
-        guid="54F949C0-DC7F-4EBC-8AE2-72BF2D0AF4EE", interaction_id=EHR_REQUEST_COMPLETED,
-    )
+    request_completed_message = build_message(guid="abc-1", interaction_id=EHR_REQUEST_COMPLETED)
     request_started_ack_message = build_message(interaction_id=APPLICATION_ACK)
     request_completed_ack_message = build_message(
-        interaction_id=APPLICATION_ACK, message_ref="54F949C0-DC7F-4EBC-8AE2-72BF2D0AF4EE",
+        interaction_id=APPLICATION_ACK, message_ref="abc-1",
     )
 
     messages = [
@@ -26,10 +24,10 @@ def test_parse_conversation_parses_a_complete_conversation():
         request_completed_ack_message,
     ]
 
-    conversation = Conversation("F8DAFCAA-5012-427B-BDB4-354256A4874B", messages)
+    conversation = Conversation("abc-0", messages)
 
     expected = ParsedConversation(
-        id="F8DAFCAA-5012-427B-BDB4-354256A4874B",
+        id="abc-0",
         request_started=request_started_message,
         request_completed=request_completed_message,
         request_completed_ack=request_completed_ack_message,
@@ -43,7 +41,7 @@ def test_parse_conversation_returns_none_when_start_omitted():
     request_completed_ack_message = build_message(interaction_id=APPLICATION_ACK)
     messages = [request_completed_ack_message]
 
-    conversation = Conversation("F8DAFCAA-5012-427B-BDB4-354256A4874B", messages)
+    conversation = Conversation("a-conversation-id", messages)
 
     expected = None
 
@@ -63,10 +61,10 @@ def test_parse_conversation_parses_incomplete_conversation():
         request_started_ack_message,
     ]
 
-    conversation = Conversation("F8DAFCAA-5012-427B-BDB4-354256A4874B", messages)
+    conversation = Conversation("abc-0", messages)
 
     expected = ParsedConversation(
-        id="F8DAFCAA-5012-427B-BDB4-354256A4874B",
+        id="abc-0",
         request_started=request_started_message,
         request_completed=request_completed_message,
         request_completed_ack=None,
