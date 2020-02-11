f0a94074051d45266e8f335981ad94b94eb5af0d
James Green <jagreen@thoughtworks.com>
2020-02-11 09:17:56 +0000

    PRMT-791 Add parser test for conversation containing large messages

--- src/gp2gp/spine/models.py
+++ src/gp2gp/spine/models.py
@@ -4,6 +4,7 @@ from typing import NamedTuple, Optional, List
 EHR_REQUEST_STARTED = "urn:nhs:names:services:gp2gp/RCMR_IN010000UK05"
 EHR_REQUEST_COMPLETED = "urn:nhs:names:services:gp2gp/RCMR_IN030000UK06"
 APPLICATION_ACK = "urn:nhs:names:services:gp2gp/MCCI_IN010000UK13"
+COMMON_POINT_TO_POINT = "urn:nhs:names:services:gp2gp/COPC_IN000001UK01"
 
 
 class Message(NamedTuple):
--- tests/unit/spine/transformers/test_parse_conversation.py
+++ tests/unit/spine/transformers/test_parse_conversation.py
@@ -2,6 +2,7 @@ from gp2gp.spine.models import (
     EHR_REQUEST_STARTED,
     EHR_REQUEST_COMPLETED,
     APPLICATION_ACK,
+    COMMON_POINT_TO_POINT,
     Conversation,
     ParsedConversation,
 )
@@ -68,6 +69,38 @@ def test_parse_conversation_parses_incomplete_conversation():
         request_started=request_started_message,
         request_completed=request_completed_message,
         request_completed_ack=None,
+    )
+    actual = parse_conversation(conversation)
+
+    assert actual == expected
+
+
+def test_parse_conversation_parses_conversation_with_large_messages():
+    request_started_message = build_message(interaction_id=EHR_REQUEST_STARTED)
+    request_completed_message = build_message(guid="abc-1", interaction_id=EHR_REQUEST_COMPLETED)
+    request_started_ack_message = build_message(interaction_id=APPLICATION_ACK)
+    common_p2p_message = build_message(guid="abc-2", interaction_id=COMMON_POINT_TO_POINT)
+    common_p2p_ack_message = build_message(interaction_id=APPLICATION_ACK, message_ref="abc-2")
+    request_completed_ack_message = build_message(
+        interaction_id=APPLICATION_ACK, message_ref="abc-1",
+    )
+
+    messages = [
+        request_started_message,
+        request_completed_message,
+        request_started_ack_message,
+        common_p2p_message,
+        common_p2p_ack_message,
+        request_completed_ack_message,
+    ]
+
+    conversation = Conversation("abc-0", messages)
+
+    expected = ParsedConversation(
+        id="abc-0",
+        request_started=request_started_message,
+        request_completed=request_completed_message,
+        request_completed_ack=request_completed_ack_message,
     )
     actual = parse_conversation(conversation)
 
