baafeb09ff151f08a840189f829d71357bbd896f
Dorota Sobkow <dorota.sobkow@thoughtworks.com>
2020-02-28 11:17:11 +0000

    PRMT-939 [DS/MP] Test construct practice list from dict with multiple practices

--- tests/unit/odsportal/sources/test_construct_practice_list_from_dict.py
+++ tests/unit/odsportal/sources/test_construct_practice_list_from_dict.py
@@ -23,3 +23,23 @@ def test_returns_list_with_one_practice():
     actual = construct_practice_list_from_dict(data)
 
     assert actual.practices == expected_practices
+
+
+def test_returns_list_with_multiple_practices():
+    data = {
+        "generated_on": "2020-07-23T00:00:00",
+        "practices": [
+            {"ods_code": "A12345", "name": "GP Practice"},
+            {"ods_code": "B12345", "name": "GP Practice 2"},
+            {"ods_code": "C12345", "name": "GP Practice 3"},
+        ],
+    }
+
+    expected_practices = [
+        PracticeDetails(ods_code="A12345", name="GP Practice"),
+        PracticeDetails(ods_code="B12345", name="GP Practice 2"),
+        PracticeDetails(ods_code="C12345", name="GP Practice 3"),
+    ]
+    actual = construct_practice_list_from_dict(data)
+
+    assert actual.practices == expected_practices
