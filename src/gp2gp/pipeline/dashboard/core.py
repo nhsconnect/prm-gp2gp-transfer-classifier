a8f4f2248343ecca90c27e0a3704dfdc17c98033
James Green <jagreen@thoughtworks.com>
2020-03-16 16:35:06 +0000

    PRMT-824 [MP/JG] Extract core of dashboard pipeline into its own package
    This allows standalone integration testing of the purely functional
    transformations that make up the core of the pipeline.
    
    The 'pipeline core' will then be available for re-use in other contexts,
    something we expect once deploying onto AWS Glue.

---
+++ src/gp2gp/pipeline/dashboard/core.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import Iterable, List
+
+from gp2gp.dashboard.transformers import construct_service_dashboard_data
+from gp2gp.odsportal.models import PracticeDetails
+from gp2gp.service.transformers import (
+    derive_transfers,
+    filter_for_successful_transfers,
+    calculate_sla_by_practice,
+)
+from gp2gp.spine.models import Message
+from gp2gp.spine.transformers import (
+    parse_conversation,
+    group_into_conversations,
+    filter_conversations_by_request_started_time,
+)
+
+
+def _parse_conversations(conversations):
+    for conversation in conversations:
+        gp2gp_conversation = parse_conversation(conversation)
+        if gp2gp_conversation is not None:
+            yield gp2gp_conversation
+
+
+def calculate_dashboard_data(
+    spine_messages: Iterable[Message],
+    start: datetime,
+    end: datetime,
+    practice_list: List[PracticeDetails],
+):
+    conversations = group_into_conversations(spine_messages)
+    parsed_conversations = _parse_conversations(conversations)
+    conversations_started_in_range = filter_conversations_by_request_started_time(
+        parsed_conversations, start, end
+    )
+    transfers = derive_transfers(conversations_started_in_range)
+    completed_transfers = filter_for_successful_transfers(transfers)
+    sla_metrics = calculate_sla_by_practice(practice_list, completed_transfers)
+    dashboard_data = construct_service_dashboard_data(
+        sla_metrics, year=start.year, month=start.month
+    )
+    return dashboard_data
--- src/gp2gp/pipeline/dashboard/main.py
+++ src/gp2gp/pipeline/dashboard/main.py
@@ -11,18 +11,8 @@ from gp2gp.io.csv import read_gzip_csv_files
 from gp2gp.io.dictionary import camelize_dict
 from gp2gp.io.json import write_json_file, read_json_file
 from gp2gp.odsportal.sources import construct_practice_list_from_dict
+from gp2gp.pipeline.dashboard.core import calculate_dashboard_data
 from gp2gp.spine.sources import construct_messages_from_splunk_items
-from gp2gp.spine.transformers import (
-    group_into_conversations,
-    parse_conversation,
-    filter_conversations_by_request_started_time,
-)
-from gp2gp.service.transformers import (
-    derive_transfers,
-    filter_for_successful_transfers,
-    calculate_sla_by_practice,
-)
-from gp2gp.dashboard.transformers import construct_service_dashboard_data
 
 
 def _list_str(values):
@@ -38,28 +28,6 @@ def parse_dashboard_pipeline_arguments(args):
     parser.add_argument("--practice-metrics-output-file", type=str)
     parser.add_argument("--practice-metadata-output-file", type=str)
     return parser.parse_args(args)
-
-
-def parse_conversations(conversations):
-    for conversation in conversations:
-        gp2gp_conversation = parse_conversation(conversation)
-        if gp2gp_conversation is not None:
-            yield gp2gp_conversation
-
-
-def process_messages(messages, start, end, practice_list):
-    conversations = group_into_conversations(messages)
-    parsed_conversations = parse_conversations(conversations)
-    conversations_started_in_range = filter_conversations_by_request_started_time(
-        parsed_conversations, start, end
-    )
-    transfers = derive_transfers(conversations_started_in_range)
-    completed_transfers = filter_for_successful_transfers(transfers)
-    sla_metrics = calculate_sla_by_practice(practice_list, completed_transfers)
-    dashboard_data = construct_service_dashboard_data(
-        sla_metrics, year=start.year, month=start.month
-    )
-    return dashboard_data
 
 
 def write_service_dashboard_json_file(dashboard_data, output_file_path):
@@ -88,7 +56,7 @@ def main():
     write_service_dashboard_json_file(practice_metadata, practice_metadata_output_file_path)
 
     spine_messages = read_spine_csv_gz_files(args.input_files)
-    service_dashboard_data = process_messages(
+    service_dashboard_data = calculate_dashboard_data(
         spine_messages, metric_month, next_month, practice_metadata.practices
     )
 
