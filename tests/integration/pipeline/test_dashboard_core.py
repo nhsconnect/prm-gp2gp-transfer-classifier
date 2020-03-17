7c9deb2015789b32ce576f3c63cba398c874835c
matthew-peers <matthew.peers@thoughtworks.com>
2020-03-17 15:15:01 +0000

    PRMT-824 [MP/JG] Enhance readability of core dashboard pipeline integration test with builder

--- tests/integration/pipeline/test_dashboard_core.py
+++ tests/integration/pipeline/test_dashboard_core.py
@@ -16,57 +16,70 @@ from tests.builders.spine import build_message
 from gp2gp.pipeline.dashboard.core import calculate_dashboard_data
 
 
+def _build_successful_conversation(**kwargs):
+    return [
+        build_message(
+            time=kwargs.get("ehr_request_started_on"),
+            conversation_id="abc",
+            guid="abc",
+            interaction_id="urn:nhs:names:services:gp2gp/RCMR_IN010000UK05",
+            from_party_ods_code=kwargs.get("requesting_ods_code"),
+            to_party_ods_code="B56789",
+        ),
+        build_message(
+            time=kwargs.get("ehr_request_completed_on"),
+            conversation_id="abc",
+            guid="abc_1",
+            interaction_id="urn:nhs:names:services:gp2gp/RCMR_IN030000UK06",
+            from_party_ods_code="B56789",
+            to_party_ods_code=kwargs.get("requesting_ods_code"),
+        ),
+        build_message(
+            time=kwargs.get("ehr_request_started_acknowledged_on"),
+            conversation_id="abc",
+            guid="abc_2",
+            interaction_id="urn:nhs:names:services:gp2gp/MCCI_IN010000UK13",
+            from_party_ods_code="B56789",
+            to_party_ods_code=kwargs.get("requesting_ods_code"),
+            message_ref="abc",
+        ),
+        build_message(
+            time=kwargs.get("ehr_request_completed_acknowledged_on"),
+            conversation_id="abc",
+            guid="abc_3",
+            interaction_id="urn:nhs:names:services:gp2gp/MCCI_IN010000UK13",
+            from_party_ods_code=kwargs.get("requesting_ods_code"),
+            to_party_ods_code="B56789",
+            message_ref="abc_1",
+        ),
+    ]
+
+
 @freeze_time(datetime(year=2020, month=1, day=15, hour=23, second=42), tz_offset=0)
 def test_calculates_correct_metrics_given_a_successful_transfer():
     time_range = DateTimeRange(
         start=datetime(2019, 12, 1, tzinfo=UTC), end=datetime(2020, 1, 1, tzinfo=UTC)
     )
 
-    spine_messages = [
-        build_message(
-            time=datetime(2019, 12, 30, 18, 2, 29, tzinfo=UTC),
-            conversation_id="abc",
-            guid="abc",
-            interaction_id="urn:nhs:names:services:gp2gp/RCMR_IN010000UK05",
-            from_party_ods_code="A12345",
-            to_party_ods_code="B56789",
-        ),
-        build_message(
-            time=datetime(2019, 12, 30, 18, 3, 21, tzinfo=UTC),
-            conversation_id="abc",
-            guid="abc_1",
-            interaction_id="urn:nhs:names:services:gp2gp/RCMR_IN030000UK06",
-            from_party_ods_code="B56789",
-            to_party_ods_code="A12345",
-        ),
-        build_message(
-            time=datetime(2019, 12, 30, 18, 3, 23, tzinfo=UTC),
-            conversation_id="abc",
-            guid="abc_2",
-            interaction_id="urn:nhs:names:services:gp2gp/MCCI_IN010000UK13",
-            from_party_ods_code="B56789",
-            to_party_ods_code="A12345",
-            message_ref="abc",
-        ),
-        build_message(
-            time=datetime(2020, 1, 1, 8, 41, 48, tzinfo=UTC),
-            conversation_id="abc",
-            guid="abc_3",
-            interaction_id="urn:nhs:names:services:gp2gp/MCCI_IN010000UK13",
-            from_party_ods_code="A12345",
-            to_party_ods_code="B56789",
-            message_ref="abc_1",
-        ),
-    ]
+    requesting_practice_name = "Test GP"
+    requesting_ods_code = "A12345"
 
-    practice_list = [PracticeDetails(ods_code="A12345", name="Test GP")]
+    spine_messages = _build_successful_conversation(
+        requesting_ods_code=requesting_ods_code,
+        ehr_request_started_on=datetime(2019, 12, 30, 18, 2, 29, tzinfo=UTC),
+        ehr_request_completed_on=datetime(2019, 12, 30, 18, 3, 21, tzinfo=UTC),
+        ehr_request_started_acknowledged_on=datetime(2019, 12, 30, 18, 3, 23, tzinfo=UTC),
+        ehr_request_completed_acknowledged_on=datetime(2020, 1, 1, 8, 41, 48, tzinfo=UTC),
+    )
+
+    practice_list = [PracticeDetails(ods_code=requesting_ods_code, name=requesting_practice_name)]
 
     expected = ServiceDashboardData(
         generated_on=datetime(year=2020, month=1, day=15, hour=23, second=42, tzinfo=UTC),
         practices=[
             PracticeSummary(
-                ods_code="A12345",
-                name="Test GP",
+                ods_code=requesting_ods_code,
+                name=requesting_practice_name,
                 metrics=[
                     MonthlyMetrics(
                         year=2019,
