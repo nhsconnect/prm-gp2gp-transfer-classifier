7aad554de2a7b22a22759d865e23a11d1bf80dd3
James Green <jagreen@thoughtworks.com>
2020-03-20 15:25:51 +0000

    PRMT-824 [MP/JG] Introduce end to end test for uploading to S3

--- tests/e2e/dashboard/test_service_dashboard_pipeline.py
+++ tests/e2e/dashboard/test_service_dashboard_pipeline.py
@@ -1,7 +1,14 @@
 import json
-import subprocess
 import gzip
 import shutil
+from io import BytesIO
+
+import boto3
+import pytest
+from botocore.config import Config
+
+from tests.builders.common import a_string
+from subprocess import PIPE, Popen
 
 
 def _read_json(path):
@@ -28,7 +35,14 @@ def _csv_join_paths(paths):
     return _csv_join([str(p) for p in paths])
 
 
-def test_service_dashboard_pipeline(datadir):
+def _read_s3_json(bucket, key):
+    f = BytesIO()
+    bucket.download_fileobj(key, f)
+    f.seek(0)
+    return json.loads(f.read().decode("utf-8"))
+
+
+def test_with_local_files(datadir):
     input_file_paths = _gzip_files(
         [datadir / "test_gp2gp_dec_2019.csv", datadir / "test_gp2gp_jan_2020.csv"]
     )
@@ -53,7 +67,7 @@ def test_service_dashboard_pipeline(datadir):
         --practice-metadata-output-file {practice_metadata_output_file_path}\
     "
 
-    process = subprocess.Popen(pipeline_command, shell=True)
+    process = Popen(pipeline_command, shell=True)
     process.wait()
 
     actual_practice_metrics = _read_json(practice_metrics_output_file_path)
@@ -61,3 +75,79 @@ def test_service_dashboard_pipeline(datadir):
 
     assert actual_practice_metrics["practices"] == expected_practice_metrics["practices"]
     assert actual_practice_metadata["practices"] == expected_practice_metadata["practices"]
+
+
+@pytest.mark.skip(reason="feature not yet implemented")
+def test_with_s3_output(datadir):
+
+    minio_data_dir = datadir / "minio"
+    minio_address = "localhost:9001"
+    minio_access_key = a_string()
+    minio_secret_key = a_string()
+    minio_region = "eu-west-2"
+    minio_command = f"\
+        minio server {minio_data_dir} \
+        --address {minio_address} \
+        --compat \
+    "
+    minio_env = {
+        "MINIO_ACCESS_KEY": minio_access_key,
+        "MINIO_SECRET_KEY": minio_secret_key,
+        "MINIO_REGION_NAME": minio_region,
+        "HOME": datadir,
+    }
+
+    minio_process = Popen(minio_command, env=minio_env, shell=True, stdout=PIPE, stderr=PIPE)
+
+    s3 = boto3.resource(
+        "s3",
+        endpoint_url=f"http://{minio_address}",
+        aws_access_key_id=minio_access_key,
+        aws_secret_access_key=minio_secret_key,
+        config=Config(signature_version="s3v4"),
+        region_name=minio_region,
+    )
+
+    output_bucket_name = "testbucket"
+    output_bucket = s3.Bucket(output_bucket_name)
+    output_bucket.create()
+
+    input_file_paths = _gzip_files(
+        [datadir / "test_gp2gp_dec_2019.csv", datadir / "test_gp2gp_jan_2020.csv"]
+    )
+    practice_metadata_file_path = datadir / "practice_metadata.json"
+    input_file_paths_str = _csv_join_paths(input_file_paths)
+
+    practice_metrics_output_key = "practice_metrics_dec_2019.json"
+    practice_metadata_output_key = "practice_metadata_dec_2019.json"
+
+    expected_practice_metrics = _read_json(datadir / "expected_practice_metrics_dec_2019.json")
+    expected_practice_metadata = _read_json(datadir / "expected_practice_metadata_dec_2019.json")
+
+    month = 12
+    year = 2019
+
+    try:
+        pipeline_command = f"\
+            gp2gp-dashboard-pipeline --month {month}\
+            --year {year}\
+            --practice-list-file {practice_metadata_file_path}\
+            --input-files {input_file_paths_str}\
+            --output-bucket {output_bucket}\
+            --practice-metrics-output-key {practice_metrics_output_key} \
+            --practice-metadata-output-key {practice_metadata_output_key} \
+        "
+
+        pipeline_process = Popen(pipeline_command, shell=True)
+        pipeline_process.wait()
+
+        actual_practice_metrics = _read_s3_json(output_bucket, practice_metrics_output_key)
+        actual_practice_metadata = _read_s3_json(output_bucket, practice_metadata_output_key)
+
+        assert actual_practice_metrics["practices"] == expected_practice_metrics["practices"]
+        assert actual_practice_metadata["practices"] == expected_practice_metadata["practices"]
+    finally:
+        output_bucket.objects.all().delete()
+        output_bucket.delete()
+        minio_process.terminate()
+        minio_process.wait()
