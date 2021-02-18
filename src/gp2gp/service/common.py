b7494af8f17ea63d1129a69070c60af9d8dc336d
helen.zhou <helen.zhou1@nhs.net>
2021-02-18 11:23:40 +0000

    PRMT-1466 [JN/HZ] Add test for assigning sla band function

--- src/gp2gp/service/common.py
+++ src/gp2gp/service/common.py
@@ -5,7 +5,13 @@ THREE_DAYS_IN_SECONDS = 259200
 EIGHT_DAYS_IN_SECONDS = 691200
 
 
-def assign_to_sla_band(sla_duration: timedelta):  # type: ignore
+class SlaBand(Enum):
+    WITHIN_3_DAYS = auto()
+    WITHIN_8_DAYS = auto()
+    BEYOND_8_DAYS = auto()
+
+
+def assign_to_sla_band(sla_duration: timedelta) -> SlaBand:
     sla_duration_in_seconds = sla_duration.total_seconds()
     if sla_duration_in_seconds <= THREE_DAYS_IN_SECONDS:
         return SlaBand.WITHIN_3_DAYS
@@ -13,9 +19,3 @@ def assign_to_sla_band(sla_duration: timedelta):  # type: ignore
         return SlaBand.WITHIN_8_DAYS
     else:
         return SlaBand.BEYOND_8_DAYS
-
-
-class SlaBand(Enum):
-    WITHIN_3_DAYS = auto()
-    WITHIN_8_DAYS = auto()
-    BEYOND_8_DAYS = auto()
---
+++ tests/unit/service/test_assign_to_sla_band.py
@@ -0,0 +1,21 @@
+import datetime
+import pytest
+
+from gp2gp.service.common import (
+    assign_to_sla_band,
+    SlaBand,
+    THREE_DAYS_IN_SECONDS,
+    EIGHT_DAYS_IN_SECONDS,
+)
+
+
+@pytest.mark.parametrize(
+    "sla_duration, expected",
+    [
+        (datetime.timedelta(seconds=THREE_DAYS_IN_SECONDS - 1), SlaBand.WITHIN_3_DAYS),
+        (datetime.timedelta(seconds=EIGHT_DAYS_IN_SECONDS), SlaBand.WITHIN_8_DAYS),
+        (datetime.timedelta(seconds=EIGHT_DAYS_IN_SECONDS + 1), SlaBand.BEYOND_8_DAYS),
+    ],
+)
+def test_return_sla_band_given_sla_duration(sla_duration, expected):
+    assert assign_to_sla_band(sla_duration=sla_duration) == expected
