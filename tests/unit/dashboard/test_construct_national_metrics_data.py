afeafdf8b309a39dd75e066db1ef64359f48f6c1
helen.zhou <helen.zhou1@nhs.net>
2021-02-18 14:23:22 +0000

    PRMT-1466 [JN/HZ] Add construction of national data platform data

---
+++ tests/unit/dashboard/test_construct_national_metrics_data.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from dateutil.tz import tzutc
+from freezegun import freeze_time
+
+from gp2gp.dashboard.nationalMetrics import construct_national_data_platform_data
+from gp2gp.service.nationalMetricsByMonth import NationalMetricsByMonth, IntegratedMetrics
+from tests.builders.common import an_integer
+
+
+def build_national_metrics_by_month(**kwargs):
+    return NationalMetricsByMonth(
+        total_count=kwargs.get("total_count", an_integer()),
+        integrated=IntegratedMetrics(
+            total_count=an_integer(),
+            within_3_days=an_integer(),
+            within_8_days=an_integer(),
+            beyond_8_days=an_integer(),
+        ),
+    )
+
+
+@freeze_time(datetime(year=2019, month=6, day=2, hour=23, second=42), tz_offset=0)
+def test_has_correct_generated_on_given_time():
+    expected_generated_on = datetime(year=2019, month=6, day=2, hour=23, second=42, tzinfo=tzutc())
+    national_metrics_by_month = build_national_metrics_by_month()
+    actual = construct_national_data_platform_data(national_metrics_by_month)
+
+    assert actual.generated_on == expected_generated_on
+
+
+def test_has_total_count_of_all_transfers():
+    expected_total_count = an_integer(2, 7)
+    national_metrics_by_month = build_national_metrics_by_month(total_count=expected_total_count)
+    actual = construct_national_data_platform_data(national_metrics_by_month)
+
+    assert actual.metrics.total_count == expected_total_count
