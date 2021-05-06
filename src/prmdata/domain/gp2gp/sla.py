7bd6f1d21876c208ae7fd48e7f975f8b7a41db9b
James Green <james.green23@nhs.net>
2021-05-06 14:49:00 +0100

    PMRT-TECH Introduce SLA counter

--- src/prmdata/domain/gp2gp/sla.py
+++ src/prmdata/domain/gp2gp/sla.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import timedelta
 from enum import Enum, auto
 
@@ -19,3 +20,24 @@ def assign_to_sla_band(sla_duration: timedelta) -> SlaBand:
         return SlaBand.WITHIN_8_DAYS
     else:
         return SlaBand.BEYOND_8_DAYS
+
+
+class SlaCounter:
+    def __init__(self):
+        self._counts = defaultdict(int)
+
+    def increment(self, duration: timedelta):
+        sla_band = assign_to_sla_band(duration)
+        self._counts[sla_band] += 1
+
+    def total(self) -> int:
+        return sum(self._counts.values())
+
+    def within_3_days(self) -> int:
+        return self._counts[SlaBand.WITHIN_3_DAYS]
+
+    def within_8_days(self):
+        return self._counts[SlaBand.WITHIN_8_DAYS]
+
+    def beyond_8_days(self):
+        return self._counts[SlaBand.BEYOND_8_DAYS]
