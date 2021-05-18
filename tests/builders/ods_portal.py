7c94c9c3126faa7d022bdc09232c007b92d2414a
James Green <79586028+jfgreen-nhs@users.noreply.github.com>
2021-05-18 17:53:48 +0100

    PRMT-TECH Remove unused builder for ods portal response

--- tests/builders/ods_portal.py
+++ tests/builders/ods_portal.py
@@ -1,5 +1,4 @@
 from random import randrange
-from unittest.mock import MagicMock
 
 from prmdata.domain.ods_portal.models import PracticeDetails
 from tests.builders.common import a_string
@@ -15,12 +14,3 @@ def build_practice_details(**kwargs):
         name=kwargs.get("name", a_string()),
         asids=kwargs.get("asids", an_asid_list()),
     )
-
-
-def build_mock_response(content=None, status_code=200, next_page=None):
-    mock_response = MagicMock()
-    mock_response.content = content
-    mock_response.status_code = status_code
-    if next_page is not None:
-        mock_response.headers = {"Next-Page": next_page}
-    return mock_response
