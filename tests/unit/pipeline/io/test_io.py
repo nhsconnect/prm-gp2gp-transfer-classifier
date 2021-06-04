ffb618610aa6455e10b2162f98965a95506da165
James Green <79586028+jfgreen-nhs@users.noreply.github.com>
2021-06-04 11:46:51 +0100

    PRMT-1798 [HZ/JG] Extract read ods metadata IO test into its own file

--- tests/unit/pipeline/io/test_io.py
+++ tests/unit/pipeline/io/test_io.py
@@ -1,24 +1,11 @@
 from typing import Iterable
 from unittest.mock import Mock, call
 
-from prmdata.domain.ods_portal.models import OrganisationMetadata
 from prmdata.domain.spine.message import Message
 from prmdata.pipeline.platform_metrics_calculator.io import PlatformMetricsIO
 from prmdata.utils.reporting_window import MonthlyReportingWindow
 from tests.builders.common import a_datetime, a_string
-from tests.builders.ods_portal import build_organisation_metadata
 from tests.builders.spine import build_message
-
-
-def _org_metadata_as_dict(metadata: OrganisationMetadata) -> dict:
-    return {
-        "generated_on": metadata.generated_on.isoformat(),
-        "practices": [
-            {"ods_code": practice.ods_code, "name": practice.name, "asids": practice.asids}
-            for practice in metadata.practices
-        ],
-        "ccgs": [{"ods_code": ccg.ods_code, "name": ccg.name} for ccg in metadata.ccgs],
-    }
 
 
 def _spine_messages_as_dict(spine_messages: Iterable[Message]) -> Iterable[dict]:
@@ -37,34 +24,6 @@ def _spine_messages_as_dict(spine_messages: Iterable[Message]) -> Iterable[dict]
         }
         for message in spine_messages
     ]
-
-
-def test_read_organisation_metadata():
-    s3_manager = Mock()
-    reporting_window = MonthlyReportingWindow.prior_to(a_datetime(year=2021, month=2))
-
-    organisation_metadata = build_organisation_metadata()
-    ods_bucket = "test_ods_bucket"
-
-    metrics_io = PlatformMetricsIO(
-        reporting_window=reporting_window,
-        s3_data_manager=s3_manager,
-        organisation_metadata_bucket=ods_bucket,
-        gp2gp_spine_bucket=a_string(),
-        dashboard_data_bucket=a_string(),
-    )
-
-    s3_manager.read_json.return_value = _org_metadata_as_dict(organisation_metadata)
-
-    expected_path = f"s3://{ods_bucket}/v2/2021/2/organisationMetadata.json"
-
-    expected_data = organisation_metadata
-
-    actual_data = metrics_io.read_ods_metadata()
-
-    assert actual_data == expected_data
-
-    s3_manager.read_json.assert_called_once_with(expected_path)
 
 
 def test_read_spine_messages():
