ee4693fc634f9ddb6d922b520a80adfef5fd5a4f
James Green <79586028+jfgreen-nhs@users.noreply.github.com>
2021-06-11 15:41:12 +0100

    PRMT-2116 [RJ/JG] Introduce optional cutoff used to ignore messages sent a certain time after the conversation began

--- src/prmdata/domain/spine/conversation.py
+++ src/prmdata/domain/spine/conversation.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import timedelta
 from typing import NamedTuple, List, Iterable, Iterator, Dict
 
 from prmdata.domain.spine.message import Message
@@ -9,13 +10,26 @@ class Conversation(NamedTuple):
     messages: List[Message]
 
 
-def group_into_conversations(messages: Iterable[Message]) -> Iterator[Conversation]:
+def _ignore_messages_sent_after(cutoff):
+    def filter_messages_outside_cutoff(messages):
+        first_message = messages[0]
+        return [message for message in messages if message.time - first_message.time <= cutoff]
+
+    return filter_messages_outside_cutoff
+
+
+def group_into_conversations(
+    message_stream: Iterable[Message], cutoff: timedelta = None
+) -> Iterator[Conversation]:
+    message_filter = None if cutoff is None else _ignore_messages_sent_after(cutoff)
     conversations: Dict[str, List[Message]] = defaultdict(list)
 
-    for message in messages:
+    for message in message_stream:
         conversations[message.conversation_id].append(message)
 
-    return (
-        Conversation(conversation_id, sorted(messages, key=lambda m: m.time))
-        for conversation_id, messages in conversations.items()
-    )
+    for conversation_id, unordered_messages in conversations.items():
+        messages = sorted(unordered_messages, key=lambda m: m.time)
+        yield Conversation(
+            conversation_id,
+            messages=messages if message_filter is None else message_filter(messages),
+        )
--- tests/unit/domain/spine/test_group_into_conversations.py
+++ tests/unit/domain/spine/test_group_into_conversations.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import pytest
 
 from prmdata.domain.spine.conversation import Conversation, group_into_conversations
 from tests.builders.spine import build_message
 
 
-def test_group_into_conversations_produces_correct_conversations():
+def test_produces_correct_conversations():
     message_one = build_message(conversation_id="abc")
     message_two = build_message(conversation_id="xyz")
     messages = [message_one, message_two]
@@ -16,7 +18,7 @@ def test_group_into_conversations_produces_correct_conversations():
     assert list(actual) == expected
 
 
-def test_group_into_conversations_produces_correct_messages_within_conversations():
+def test_produces_correct_messages_within_conversations():
     message_one = build_message(conversation_id="abc", time=datetime(year=2020, month=6, day=5))
     message_two = build_message(conversation_id="abc", time=datetime(year=2020, month=6, day=6))
     messages = [message_one, message_two]
@@ -28,7 +30,7 @@ def test_group_into_conversations_produces_correct_messages_within_conversations
     assert list(actual) == expected
 
 
-def test_group_into_conversations_sorts_messages_within_conversations():
+def test_sorts_messages_within_conversations():
     message_one = build_message(conversation_id="abc", time=datetime(year=2020, month=6, day=6))
     message_two = build_message(conversation_id="abc", time=datetime(year=2020, month=6, day=5))
     messages = [message_one, message_two]
@@ -36,5 +38,39 @@ def test_group_into_conversations_sorts_messages_within_conversations():
     expected = [Conversation("abc", [message_two, message_one])]
 
     actual = group_into_conversations(messages)
+
+    assert list(actual) == expected
+
+
+@pytest.mark.parametrize(
+    ["cutoff_interval", "expected_message_ids"],
+    [
+        (timedelta(days=0, hours=0), ["1"]),
+        (timedelta(days=0, hours=12), ["1"]),
+        (timedelta(days=1, hours=12), ["1", "2"]),
+        (timedelta(days=2, hours=12), ["1", "2", "3"]),
+    ],
+)
+def test_rejects_messages_after_cutoff(cutoff_interval, expected_message_ids):
+    messages = [
+        build_message(conversation_id="a", guid="1", time=datetime(year=2020, month=6, day=6)),
+        build_message(conversation_id="a", guid="2", time=datetime(year=2020, month=6, day=7)),
+        build_message(conversation_id="a", guid="3", time=datetime(year=2020, month=6, day=8)),
+    ]
+
+    conversations = group_into_conversations(messages, cutoff=cutoff_interval)
+    actual_message_ids = [m.guid for m in next(conversations).messages]
+
+    assert actual_message_ids == expected_message_ids
+
+
+def test_has_no_cutoff_by_default():
+    message_one = build_message(conversation_id="abc", time=datetime(year=2020, month=6, day=6))
+    message_two = build_message(conversation_id="abc", time=datetime(year=2060, month=6, day=6))
+    messages = [message_one, message_two]
+
+    expected = [Conversation("abc", [message_one, message_two])]
+
+    actual = group_into_conversations(messages, cutoff=None)
 
     assert list(actual) == expected
