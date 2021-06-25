b0bbd6dab701a4507f7276673dc674d03149df09
James Green <79586028+jfgreen-nhs@users.noreply.github.com>
2021-06-25 12:52:35 +0100

    PRMT-TECH use common case when testing effective final ack time given request made

--- tests/unit/domain/spine/gp2gp_conversation/test_effective_final_acknowledgement_time.py
+++ tests/unit/domain/spine/gp2gp_conversation/test_effective_final_acknowledgement_time.py
@@ -1,15 +1,16 @@
-from prmdata.domain.spine.message import ERROR_SUPPRESSED
+from typing import List
+
+from prmdata.domain.spine.gp2gp_conversation import Gp2gpConversation
+from prmdata.domain.spine.message import ERROR_SUPPRESSED, Message
+from tests.builders import test_cases
 from tests.builders.common import a_datetime
 from tests.builders.spine import build_gp2gp_conversation, build_message
 
 
 def test_returns_none_when_request_has_been_made():
-    conversation = build_gp2gp_conversation(
-        request_started=build_message(),
-        request_started_ack=None,
-        request_completed_messages=[],
-        request_completed_ack_messages=[],
-    )
+    gp2gp_messages: List[Message] = test_cases.gp2gp_request_made()
+
+    conversation = Gp2gpConversation.from_messages(gp2gp_messages)
 
     expected = None
 
