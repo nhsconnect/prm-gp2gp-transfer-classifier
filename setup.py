e2031be60db5d49f9e74955c432a944d1b573144
helen.zhou <helen.zhou1@nhs.net>
2021-07-29 11:23:11 +0100

    PRMT-2270 [JN/HZ] Rename package name to gp2gp-transfer-classifier

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="gp2gp-data-pipeline",
+    name="gp2gp-transfer-classifier",
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
