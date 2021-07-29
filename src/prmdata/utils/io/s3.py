258e0ce289128ed07df97b1fc60ba71b578d9827
helen.zhou <helen.zhou1@nhs.net>
2021-07-29 15:23:43 +0100

    PRMT-2270 [HZ/JN] Remove write and read JSON methods

--- src/prmdata/utils/io/s3.py
+++ src/prmdata/utils/io/s3.py
@@ -1,14 +1,6 @@
 import csv
 import gzip
-import json
-from datetime import datetime
 from urllib.parse import urlparse
-
-
-def _serialize_datetime(obj):
-    if isinstance(obj, datetime):
-        return obj.isoformat()
-    raise TypeError(f"Type {type(obj)} is not JSON serializable")
 
 
 class S3DataManager:
@@ -20,17 +12,6 @@ class S3DataManager:
         s3_bucket = object_url.netloc
         s3_key = object_url.path.lstrip("/")
         return self._client.Object(s3_bucket, s3_key)
-
-    def read_json(self, object_uri):
-        s3_object = self._object_from_uri(object_uri)
-        response = s3_object.get()
-        body = response["Body"].read()
-        return json.loads(body.decode("utf8"))
-
-    def write_json(self, object_uri, data):
-        s3_object = self._object_from_uri(object_uri)
-        body = json.dumps(data, default=_serialize_datetime).encode("utf8")
-        s3_object.put(Body=body, ContentType="application/json")
 
     def read_gzip_csv(self, object_uri):
         s3_object = self._object_from_uri(object_uri)
