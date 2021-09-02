c96a6704278e25de2fe272824820fa72e9acfb1b
helen.zhou <helen.zhou1@nhs.net>
2021-09-02 16:28:19 +0100

    PRMT-2320 [HZ/KV] Add test for json formatter

---
+++ tests/unit/utils/io/test_logging.py
@@ -0,0 +1,29 @@
+import json
+from logging import makeLogRecord
+
+from prmdata.pipeline.io.logging import JsonFormatter
+
+
+def test_json_formatter_correctly_formats_record():
+    record = makeLogRecord(
+        {
+            "levelname": "warning",
+            "msg": "a message",
+            "module": "a.module",
+            "created": 1607965513.358049,
+            "extra_field": "some_value",
+        }
+    )
+
+    expected = {
+        "level": "warning",
+        "message": "a message",
+        "module": "a.module",
+        "time": "2020-12-14T17:05:13.358049",
+        "extra_field": "some_value",
+    }
+
+    actual_json_string = JsonFormatter().format(record)
+    actual = json.loads(actual_json_string)
+
+    assert actual == expected
