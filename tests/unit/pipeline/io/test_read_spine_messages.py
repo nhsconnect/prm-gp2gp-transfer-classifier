c92e76f4c47073f634732bae44f18ed5ab932ad0
James Green <79586028+jfgreen-nhs@users.noreply.github.com>
2021-10-06 16:27:27 +0100

    PRMT-TECH Enhance clarity of spine message reading test

--- tests/unit/pipeline/io/test_read_spine_messages.py
+++ tests/unit/pipeline/io/test_read_spine_messages.py
@@ -1,57 +1,105 @@
-from collections import OrderedDict
+from datetime import datetime
 
+from dateutil.tz import tzutc
+
+from prmdata.domain.spine.message import Message
 from prmdata.pipeline.io import TransferClassifierIO
 from prmdata.utils.io.s3 import S3DataManager
+from tests.builders.common import a_string
 from tests.builders.file import build_gzip_csv
 from tests.builders.s3 import MockS3Object, MockS3
-from tests.builders.spine import build_message
+from tests.builders.spine import build_spine_item
 
-_SPINE_CSV_MAPPING = OrderedDict(
-    [
-        ("_time", lambda m: m.time.isoformat()),
-        ("conversationID", lambda m: m.conversation_id),
-        ("GUID", lambda m: m.guid),
-        ("interactionID", lambda m: m.interaction_id),
-        ("messageSender", lambda m: m.from_party_asid),
-        ("messageRecipient", lambda m: m.to_party_asid),
-        ("messageRef", lambda m: "NotProvided" if m.message_ref is None else m.message_ref),
-        ("fromSystem", lambda m: m.from_system),
-        ("toSystem", lambda m: m.to_system),
-        ("jdiEvent", lambda m: "NONE" if m.error_code is None else str(m.error_code)),
-    ]
-)
+_SPINE_CSV_COLUMNS = [
+    "_time",
+    "conversationID",
+    "GUID",
+    "interactionID",
+    "messageSender",
+    "messageRecipient",
+    "messageRef",
+    "fromSystem",
+    "toSystem",
+    "jdiEvent",
+]
 
 
-def _spine_csv_gz(messages):
+def _spine_csv_gz(rows):
     return build_gzip_csv(
-        header=_SPINE_CSV_MAPPING.keys(),
-        rows=[[field(msg) for field in _SPINE_CSV_MAPPING.values()] for msg in messages],
+        header=_SPINE_CSV_COLUMNS,
+        rows=[[row.get(field, a_string()) for field in _SPINE_CSV_COLUMNS] for row in rows],
     )
 
 
-def test_read_spine_messages():
+def test_read_spine_messages_reads_single_message_correctly():
+    csv_row = build_spine_item(
+        time="2019-12-31T23:37:55.334+0000",
+        conversation_id="abc",
+        guid="message_a",
+        interaction_id="an_interaction_id",
+        message_sender="sender_x",
+        message_recipient="recipient_y",
+        message_ref="NotProvided",
+        jdi_event="NONE",
+        raw="",
+        from_system="SupplierA",
+        to_system="Unknown",
+    )
 
-    spine_messages = [build_message() for _ in range(10)]
+    mock_s3_conn = MockS3(
+        objects=[
+            MockS3Object(
+                bucket="test_bucket", key="data/1.csv.gz", contents=_spine_csv_gz([csv_row])
+            )
+        ]
+    )
+
+    io = TransferClassifierIO(s3_data_manager=S3DataManager(mock_s3_conn))
+
+    expected_spine_message = Message(
+        time=datetime(2019, 12, 31, 23, 37, 55, 334000, tzutc()),
+        conversation_id="abc",
+        guid="message_a",
+        interaction_id="an_interaction_id",
+        from_party_asid="sender_x",
+        to_party_asid="recipient_y",
+        message_ref=None,
+        error_code=None,
+        from_system="SupplierA",
+        to_system="Unknown",
+    )
+
+    actual = io.read_spine_messages(["s3://test_bucket/data/1.csv.gz"])
+
+    assert list(actual) == [expected_spine_message]
+
+
+def test_read_spine_messages_reads_multiple_messages():
+    csv_rows = [build_spine_item(guid=f"guid{i}") for i in range(10)]
 
     mock_s3_conn = MockS3(
         objects=[
             MockS3Object(
                 bucket="test_bucket",
                 key="data/1.csv.gz",
-                contents=_spine_csv_gz(messages=spine_messages[:4]),
+                contents=_spine_csv_gz(csv_rows[:4]),
             ),
             MockS3Object(
                 bucket="test_bucket",
                 key="data/2.csv.gz",
-                contents=_spine_csv_gz(messages=spine_messages[4:]),
+                contents=_spine_csv_gz(csv_rows[4:]),
             ),
         ]
     )
 
     io = TransferClassifierIO(s3_data_manager=S3DataManager(mock_s3_conn))
 
-    actual = io.read_spine_messages(
+    expected_guids = [f"guid{i}" for i in range(10)]
+
+    actual_messages = io.read_spine_messages(
         ["s3://test_bucket/data/1.csv.gz", "s3://test_bucket/data/2.csv.gz"]
     )
 
-    assert list(actual) == spine_messages
+    actual_guids = [message.guid for message in actual_messages]
+
+    assert actual_guids == expected_guids
