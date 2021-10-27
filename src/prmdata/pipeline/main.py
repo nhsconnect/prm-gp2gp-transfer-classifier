d4ae229113f9af87a663b84fb791af703a04c308
James Green <79586028+jfgreen-nhs@users.noreply.github.com>
2021-10-27 11:57:06 +0100

    PRMT-TECH Introduce class responsible for co-ordinating pipline

--- src/prmdata/pipeline/main.py
+++ src/prmdata/pipeline/main.py
@@ -24,45 +24,53 @@ def _setup_logger():
     logger.addHandler(handler)
 
 
+class TransferClassifierPipeline:
+    def __init__(self, config: DataPipelineConfig):
+        s3 = boto3.resource("s3", endpoint_url=config.s3_endpoint_url)
+        s3_manager = S3DataManager(s3)
+
+        self._reporting_window = MonthlyReportingWindow.prior_to(config.date_anchor)
+
+        self._output_metadata = {
+            "date-anchor": config.date_anchor.isoformat(),
+            "cutoff-days": str(config.conversation_cutoff.days),
+            "build-tag": config.build_tag,
+        }
+
+        self._cutoff = config.conversation_cutoff
+
+        self._uris = TransferClassifierS3UriResolver(
+            gp2gp_spine_bucket=config.input_spine_data_bucket,
+            transfers_bucket=config.output_transfer_data_bucket,
+        )
+
+        self._io = TransferClassifierIO(s3_manager)
+
+    def _read_spine_messages(self):
+        input_path = self._uris.spine_messages(self._reporting_window)
+        return self._io.read_spine_messages(input_path)
+
+    def _write_transfers(self, transfers):
+        output_path = self._uris.gp2gp_transfers(self._reporting_window)
+        self._io.write_transfers(transfers, output_path, self._output_metadata)
+
+    def run(self):
+        spine_messages = self._read_spine_messages()
+        transfer_observability_probe = TransferObservabilityProbe(logger=module_logger)
+        transfers = parse_transfers_from_messages(
+            spine_messages=spine_messages,
+            reporting_window=self._reporting_window,
+            conversation_cutoff=self._cutoff,
+            observability_probe=transfer_observability_probe,
+        )
+        self._write_transfers(transfers)
+
+
 def main():
     _setup_logger()
-
     config = DataPipelineConfig.from_environment_variables(environ)
-
-    s3 = boto3.resource("s3", endpoint_url=config.s3_endpoint_url)
-    s3_manager = S3DataManager(s3)
-
-    reporting_window = MonthlyReportingWindow.prior_to(config.date_anchor)
-
-    uris = TransferClassifierS3UriResolver(
-        gp2gp_spine_bucket=config.input_spine_data_bucket,
-        transfers_bucket=config.output_transfer_data_bucket,
-    )
-
-    io = TransferClassifierIO(s3_manager)
-
-    input_path = uris.spine_messages(reporting_window)
-    spine_messages = io.read_spine_messages(input_path)
-
-    conversation_cutoff = config.conversation_cutoff
-
-    transfer_observability_probe = TransferObservabilityProbe(logger=module_logger)
-    transfers = parse_transfers_from_messages(
-        spine_messages=spine_messages,
-        reporting_window=reporting_window,
-        conversation_cutoff=conversation_cutoff,
-        observability_probe=transfer_observability_probe,
-    )
-
-    output_metadata = {
-        "date-anchor": config.date_anchor.isoformat(),
-        "cutoff-days": str(config.conversation_cutoff.days),
-        "build-tag": config.build_tag,
-    }
-
-    output_path = uris.gp2gp_transfers(reporting_window)
-
-    io.write_transfers(transfers, output_path, output_metadata)
+    pipeline = TransferClassifierPipeline(config)
+    pipeline.run()
 
 
 if __name__ == "__main__":
