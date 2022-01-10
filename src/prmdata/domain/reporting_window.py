12746de8d0ba611be3ed7e7fab7e10db14802ed5
helen.zhou <helen.zhou1@nhs.net>
2022-01-10 10:46:54 +0000

    PRMT-2330 [HZ/AB] Add get_overflow_dates to reporting window

--- src/prmdata/domain/reporting_window.py
+++ src/prmdata/domain/reporting_window.py
@@ -1,12 +1,20 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List
 
 from prmdata.utils.date_converter import date_range_to_dates_converter
 
 
 class ReportingWindow:
-    def __init__(self, start_datetime: datetime, end_datetime: datetime):
+    def __init__(
+        self, start_datetime: datetime, end_datetime: datetime, conversation_cutoff: timedelta
+    ):
         self._dates = date_range_to_dates_converter(start_datetime, end_datetime)
+
+        cutoff_datetime = end_datetime + conversation_cutoff
+        self._overflow_dates = date_range_to_dates_converter(end_datetime, cutoff_datetime)
 
     def get_dates(self) -> List[datetime]:
         return self._dates
+
+    def get_overflow_dates(self) -> List[datetime]:
+        return self._overflow_dates
--- tests/unit/domain/test_reporting_window.py
+++ tests/unit/domain/test_reporting_window.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from prmdata.domain.reporting_window import ReportingWindow
 
@@ -6,8 +6,9 @@ from prmdata.domain.reporting_window import ReportingWindow
 def test_get_dates_returns_list_of_datetimes_within_reporting_window():
     start_datetime = datetime(year=2021, month=12, day=30)
     end_datetime = datetime(year=2022, month=1, day=3)
+    conversation_cutoff = timedelta(days=14)
 
-    reporting_window = ReportingWindow(start_datetime, end_datetime)
+    reporting_window = ReportingWindow(start_datetime, end_datetime, conversation_cutoff)
 
     expected = [
         datetime(year=2021, month=12, day=30),
@@ -19,3 +20,21 @@ def test_get_dates_returns_list_of_datetimes_within_reporting_window():
     actual = reporting_window.get_dates()
 
     assert actual == expected
+
+
+def test_get_overflow_dates_returns_list_of_datetimes_within_cutoff_period():
+    start_datetime = datetime(year=2019, month=12, day=30, hour=0, minute=0, second=0)
+    end_datetime = datetime(year=2019, month=12, day=31, hour=0, minute=0, second=0)
+    conversation_cutoff = timedelta(days=3)
+
+    reporting_window = ReportingWindow(start_datetime, end_datetime, conversation_cutoff)
+
+    expected_overflow_dates = [
+        datetime(year=2019, month=12, day=31),
+        datetime(year=2020, month=1, day=1),
+        datetime(year=2020, month=1, day=2),
+    ]
+
+    actual = reporting_window.get_overflow_dates()
+
+    assert actual == expected_overflow_dates
