66f3071a92c4712986c76f9b0e1ccbc8860b867a
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-01-12 12:02:00 +0000

    PRMT-2330 [HZ/SP] Add ability to parse transfers from messages by a specified day

--- src/prmdata/pipeline/parse_transfers_from_messages.py
+++ src/prmdata/pipeline/parse_transfers_from_messages.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 from logging import getLogger
 from typing import Iterable, Iterator, Union
 
@@ -6,7 +6,10 @@ from prmdata.domain.gp2gp.transfer import Transfer
 from prmdata.domain.gp2gp.transfer_service import TransferObservabilityProbe, TransferService
 from prmdata.domain.monthly_reporting_window import MonthlyReportingWindow
 from prmdata.domain.reporting_window import ReportingWindow
-from prmdata.domain.spine.gp2gp_conversation import filter_conversations_by_request_started_time
+from prmdata.domain.spine.gp2gp_conversation import (
+    filter_conversations_by_day,
+    filter_conversations_by_request_started_time,
+)
 from prmdata.domain.spine.message import Message
 
 module_logger = getLogger(__name__)
@@ -30,6 +33,29 @@ def parse_transfers_from_messages_monthly(
     )
     conversations_started_in_reporting_window = filter_conversations_by_request_started_time(
         gp2gp_conversations, reporting_window
+    )
+
+    return transfer_service.convert_to_transfers(conversations_started_in_reporting_window)
+
+
+def parse_transfers_from_messages(
+    spine_messages: Iterable[Message],
+    daily_start_datetime: datetime,
+    conversation_cutoff: timedelta,
+    observability_probe: TransferObservabilityProbe,
+) -> Iterator[Transfer]:
+    transfer_service = TransferService(
+        message_stream=spine_messages,
+        cutoff=conversation_cutoff,
+        observability_probe=observability_probe,
+    )
+
+    conversations = transfer_service.group_into_conversations()
+    gp2gp_conversations = transfer_service.parse_conversations_into_gp2gp_conversations(
+        conversations
+    )
+    conversations_started_in_reporting_window = filter_conversations_by_day(
+        gp2gp_conversations, daily_start_datetime
     )
 
     return transfer_service.convert_to_transfers(conversations_started_in_reporting_window)
--- tests/integration/pipeline/test_core_parse_transfers_from_messages.py
+++ tests/integration/pipeline/test_core_parse_transfers_from_messages.py
@@ -6,7 +6,10 @@ from dateutil.tz import UTC
 from prmdata.domain.gp2gp.transfer import Practice, Transfer
 from prmdata.domain.gp2gp.transfer_outcome import TransferOutcome, TransferStatus
 from prmdata.domain.monthly_reporting_window import MonthlyReportingWindow
-from prmdata.pipeline.parse_transfers_from_messages import parse_transfers_from_messages_monthly
+from prmdata.pipeline.parse_transfers_from_messages import (
+    parse_transfers_from_messages,
+    parse_transfers_from_messages_monthly,
+)
 from tests.builders.spine import build_message
 
 mock_probe = Mock()
@@ -59,7 +62,7 @@ def _build_successful_conversation(**kwargs):
     ]
 
 
-def test_parses_transfer_correctly_given_valid_message_list():
+def test_returns_transfers_given_valid_message_list():
     reporting_window = MonthlyReportingWindow(
         metric_month_start=datetime(2019, 12, 1, tzinfo=UTC),
         overflow_month_start=datetime(2020, 1, 1, tzinfo=UTC),
@@ -101,6 +104,54 @@ def test_parses_transfer_correctly_given_valid_message_list():
         parse_transfers_from_messages_monthly(
             spine_messages=spine_messages,
             reporting_window=reporting_window,
+            conversation_cutoff=timedelta(14),
+            observability_probe=mock_probe,
+        )
+    )
+
+    assert actual == expected
+
+
+def test_returns_transfers_given_valid_message_list_within_day():
+    daily_start_datetime = datetime(year=2019, month=12, day=30, tzinfo=UTC)
+
+    conversation_id = "abcdefg_1234"
+    requesting_practice = Practice(asid="343434343434", supplier="SupplierA")
+    sending_practice = Practice(asid="111134343434", supplier="SupplierB")
+
+    spine_messages = _build_successful_conversation(
+        conversation_id=conversation_id,
+        requesting_asid=requesting_practice.asid,
+        sending_asid=sending_practice.asid,
+        requesting_supplier=requesting_practice.supplier,
+        sending_supplier=sending_practice.supplier,
+        ehr_request_started_on=datetime(2019, 12, 30, 18, 2, 29, tzinfo=UTC),
+        # core ehr/request completed sometimes gets sent before request started acknowledgement
+        ehr_request_completed_on=datetime(2019, 12, 30, 18, 3, 21, tzinfo=UTC),
+        ehr_request_started_acknowledged_on=datetime(2019, 12, 30, 18, 3, 23, tzinfo=UTC),
+        ehr_request_completed_acknowledged_on=datetime(2019, 12, 30, 20, 41, 48, tzinfo=UTC),
+    )
+
+    expected = [
+        Transfer(
+            conversation_id=conversation_id,
+            sla_duration=timedelta(seconds=9507),
+            requesting_practice=requesting_practice,
+            sending_practice=sending_practice,
+            outcome=TransferOutcome(failure_reason=None, status=TransferStatus.INTEGRATED_ON_TIME),
+            date_requested=datetime(2019, 12, 30, 18, 2, 29, tzinfo=UTC),
+            date_completed=datetime(2019, 12, 30, 20, 41, 48, tzinfo=UTC),
+            sender_error_codes=[None],
+            final_error_codes=[None],
+            intermediate_error_codes=[],
+            last_sender_message_timestamp=datetime(2019, 12, 30, 18, 3, 23, tzinfo=UTC),
+        )
+    ]
+
+    actual = list(
+        parse_transfers_from_messages(
+            spine_messages=spine_messages,
+            daily_start_datetime=daily_start_datetime,
             conversation_cutoff=timedelta(14),
             observability_probe=mock_probe,
         )
