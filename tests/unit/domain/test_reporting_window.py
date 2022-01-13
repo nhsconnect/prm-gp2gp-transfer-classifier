89e6d6562d25378ecc7a8f6880f33978e38d7ba1
helen.zhou <helen.zhou1@nhs.net>
2022-01-13 12:20:28 +0000

    PRMT-2330 [HZ/SP] Add UTC to reporting window tests and add 0 cut off test

--- tests/unit/domain/test_reporting_window.py
+++ tests/unit/domain/test_reporting_window.py
@@ -1,20 +1,23 @@
 from datetime import datetime, timedelta
+from typing import List
+
+from dateutil.tz import UTC
 
 from prmdata.domain.reporting_window import ReportingWindow
 
 
 def test_get_dates_returns_list_of_datetimes_within_reporting_window():
-    start_datetime = datetime(year=2021, month=12, day=30)
-    end_datetime = datetime(year=2022, month=1, day=3)
+    start_datetime = datetime(year=2021, month=12, day=30, tzinfo=UTC)
+    end_datetime = datetime(year=2022, month=1, day=3, tzinfo=UTC)
     conversation_cutoff = timedelta(days=14)
 
     reporting_window = ReportingWindow(start_datetime, end_datetime, conversation_cutoff)
 
     expected = [
-        datetime(year=2021, month=12, day=30),
-        datetime(year=2021, month=12, day=31),
-        datetime(year=2022, month=1, day=1),
-        datetime(year=2022, month=1, day=2),
+        datetime(year=2021, month=12, day=30, tzinfo=UTC),
+        datetime(year=2021, month=12, day=31, tzinfo=UTC),
+        datetime(year=2022, month=1, day=1, tzinfo=UTC),
+        datetime(year=2022, month=1, day=2, tzinfo=UTC),
     ]
 
     actual = reporting_window.get_dates()
@@ -23,17 +26,31 @@ def test_get_dates_returns_list_of_datetimes_within_reporting_window():
 
 
 def test_get_overflow_dates_returns_list_of_datetimes_within_cutoff_period():
-    start_datetime = datetime(year=2019, month=12, day=30, hour=0, minute=0, second=0)
-    end_datetime = datetime(year=2019, month=12, day=31, hour=0, minute=0, second=0)
+    start_datetime = datetime(year=2019, month=12, day=30, hour=0, minute=0, second=0, tzinfo=UTC)
+    end_datetime = datetime(year=2019, month=12, day=31, hour=0, minute=0, second=0, tzinfo=UTC)
     conversation_cutoff = timedelta(days=3)
 
     reporting_window = ReportingWindow(start_datetime, end_datetime, conversation_cutoff)
 
     expected_overflow_dates = [
-        datetime(year=2019, month=12, day=31),
-        datetime(year=2020, month=1, day=1),
-        datetime(year=2020, month=1, day=2),
+        datetime(year=2019, month=12, day=31, tzinfo=UTC),
+        datetime(year=2020, month=1, day=1, tzinfo=UTC),
+        datetime(year=2020, month=1, day=2, tzinfo=UTC),
     ]
+
+    actual = reporting_window.get_overflow_dates()
+
+    assert actual == expected_overflow_dates
+
+
+def test_returns_empty_list_given_0_cutoff():
+    start_datetime = datetime(year=2022, month=1, day=12, hour=0, minute=0, second=0, tzinfo=UTC)
+    end_datetime = datetime(year=2022, month=1, day=13, hour=0, minute=0, second=0, tzinfo=UTC)
+    conversation_cutoff = timedelta(days=0)
+
+    reporting_window = ReportingWindow(start_datetime, end_datetime, conversation_cutoff)
+
+    expected_overflow_dates: List = []
 
     actual = reporting_window.get_overflow_dates()
 
