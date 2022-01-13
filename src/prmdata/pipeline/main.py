9db341f7441ee99ed5436100d3d1af04f90a213d
helen.zhou <helen.zhou1@nhs.net>
2022-01-13 11:58:30 +0000

    PRMT-2330 [HZ/SP] Move Transfer Classifier (daily and monthly) classes into separate files

--- src/prmdata/pipeline/main.py
+++ src/prmdata/pipeline/main.py
@@ -1,29 +1,10 @@
 import logging
-from datetime import datetime, timedelta
 from os import environ
-from typing import Iterator
 
-import boto3
-
-from prmdata.domain.gp2gp.transfer import Transfer
-from prmdata.domain.gp2gp.transfer_service import (
-    TransferObservabilityProbe,
-    TransferService,
-    module_logger,
-)
-from prmdata.domain.monthly_reporting_window import MonthlyReportingWindow
-from prmdata.domain.reporting_window import ReportingWindow
-from prmdata.domain.spine.gp2gp_conversation import filter_conversations_by_day
-from prmdata.domain.spine.message import Message
 from prmdata.pipeline.config import TransferClassifierConfig
-from prmdata.pipeline.io import (
-    TransferClassifierIO,
-    TransferClassifierMonthlyS3UriResolver,
-    TransferClassifierS3UriResolver,
-)
-from prmdata.pipeline.parse_transfers_from_messages import parse_transfers_from_messages_monthly
+from prmdata.pipeline.transfer_classifier import TransferClassifier
+from prmdata.pipeline.transfer_classifier_monthly import TransferClassifierMonthly
 from prmdata.utils.io.json_formatter import JsonFormatter
-from prmdata.utils.io.s3 import S3DataManager
 
 logger = logging.getLogger("prmdata")
 
@@ -36,120 +17,12 @@ def _setup_logger():
     logger.addHandler(handler)
 
 
-class TransferClassifierMonthlyPipeline:
-    def __init__(self, config: TransferClassifierConfig):
-        s3 = boto3.resource("s3", endpoint_url=config.s3_endpoint_url)
-        s3_manager = S3DataManager(s3)
-
-        self._reporting_window = MonthlyReportingWindow.prior_to(config.date_anchor)
-
-        self._cutoff = config.conversation_cutoff
-
-        self._uris = TransferClassifierMonthlyS3UriResolver(
-            gp2gp_spine_bucket=config.input_spine_data_bucket,
-            transfers_bucket=config.output_transfer_data_bucket,
-        )
-
-        output_metadata = {
-            "date-anchor": config.date_anchor.isoformat(),
-            "cutoff-days": str(config.conversation_cutoff.days),
-            "build-tag": config.build_tag,
-        }
-
-        self._io = TransferClassifierIO(s3_manager, output_metadata)
-
-    def _read_spine_messages(self, metric_month, overflow_month):
-        input_path = self._uris.spine_messages(metric_month, overflow_month)
-        return self._io.read_spine_messages(input_path)
-
-    def _write_transfers(self, transfers, metric_month):
-        output_path = self._uris.gp2gp_transfers(metric_month)
-        self._io.write_transfers(transfers, output_path)
-
-    def run(self):
-        metric_month = self._reporting_window.metric_month
-        overflow_month = self._reporting_window.overflow_month
-        spine_messages = self._read_spine_messages(metric_month, overflow_month)
-        transfer_observability_probe = TransferObservabilityProbe(logger=module_logger)
-        transfers = parse_transfers_from_messages_monthly(
-            spine_messages=spine_messages,
-            reporting_window=self._reporting_window,
-            conversation_cutoff=self._cutoff,
-            observability_probe=transfer_observability_probe,
-        )
-        self._write_transfers(transfers, metric_month)
-
-
-class TransferClassifierPipeline:
-    def __init__(self, config: TransferClassifierConfig):
-        s3 = boto3.resource("s3", endpoint_url=config.s3_endpoint_url)
-        s3_manager = S3DataManager(s3)
-
-        self._reporting_window = ReportingWindow(
-            config.start_datetime, config.end_datetime, config.conversation_cutoff
-        )
-
-        self._cutoff = config.conversation_cutoff
-
-        self._uris = TransferClassifierS3UriResolver(
-            gp2gp_spine_bucket=config.input_spine_data_bucket,
-            transfers_bucket=config.output_transfer_data_bucket,
-        )
-
-        output_metadata = {
-            "cutoff-days": str(config.conversation_cutoff.days),
-            "build-tag": config.build_tag,
-            "start-datetime": config.start_datetime.isoformat(),
-            "end-datetime": config.end_datetime.isoformat(),
-        }
-
-        self._io = TransferClassifierIO(s3_manager, output_metadata)
-
-    def _read_spine_messages(self) -> Iterator[Message]:
-        input_paths = self._uris.spine_messages(self._reporting_window)
-        return self._io.read_spine_messages(input_paths)
-
-    def _write_transfers(
-        self, transfers: Iterator[Transfer], daily_start_datetime: datetime, cutoff: timedelta
-    ):
-        output_path = self._uris.gp2gp_transfers(
-            daily_start_datetime=daily_start_datetime, cutoff=cutoff
-        )
-        self._io.write_transfers(transfers, output_path)
-
-    def run(self):
-        transfer_observability_probe = TransferObservabilityProbe(logger=module_logger)
-        spine_messages = self._read_spine_messages()
-
-        transfer_service = TransferService(
-            message_stream=spine_messages,
-            cutoff=self._cutoff,
-            observability_probe=transfer_observability_probe,
-        )
-
-        conversations = transfer_service.group_into_conversations()
-        gp2gp_conversations = transfer_service.parse_conversations_into_gp2gp_conversations(
-            conversations
-        )
-
-        for daily_start_datetime in self._reporting_window.get_dates():
-            conversations_started_in_reporting_window = filter_conversations_by_day(
-                gp2gp_conversations, daily_start_datetime
-            )
-            transfers = transfer_service.convert_to_transfers(
-                conversations_started_in_reporting_window
-            )
-            self._write_transfers(
-                transfers=transfers, daily_start_datetime=daily_start_datetime, cutoff=self._cutoff
-            )
-
-
 def main():
     _setup_logger()
     config = TransferClassifierConfig.from_environment_variables(environ)
-    TransferClassifierPipeline(
+    TransferClassifier(config).run() if config.date_anchor is None else TransferClassifierMonthly(
         config
-    ).run() if config.date_anchor is None else TransferClassifierMonthlyPipeline(config).run()
+    ).run()
 
 
 if __name__ == "__main__":
---
+++ src/prmdata/pipeline/transfer_classifier.py
@@ -0,0 +1,81 @@
+from datetime import datetime, timedelta
+from typing import Iterator
+
+import boto3
+
+from prmdata.domain.gp2gp.transfer import Transfer
+from prmdata.domain.gp2gp.transfer_service import (
+    TransferObservabilityProbe,
+    TransferService,
+    module_logger,
+)
+from prmdata.domain.reporting_window import ReportingWindow
+from prmdata.domain.spine.gp2gp_conversation import filter_conversations_by_day
+from prmdata.domain.spine.message import Message
+from prmdata.pipeline.config import TransferClassifierConfig
+from prmdata.pipeline.io import TransferClassifierIO, TransferClassifierS3UriResolver
+from prmdata.utils.io.s3 import S3DataManager
+
+
+class TransferClassifier:
+    def __init__(self, config: TransferClassifierConfig):
+        s3 = boto3.resource("s3", endpoint_url=config.s3_endpoint_url)
+        s3_manager = S3DataManager(s3)
+
+        self._reporting_window = ReportingWindow(
+            config.start_datetime, config.end_datetime, config.conversation_cutoff
+        )
+
+        self._cutoff = config.conversation_cutoff
+
+        self._uris = TransferClassifierS3UriResolver(
+            gp2gp_spine_bucket=config.input_spine_data_bucket,
+            transfers_bucket=config.output_transfer_data_bucket,
+        )
+
+        output_metadata = {
+            "cutoff-days": str(config.conversation_cutoff.days),
+            "build-tag": config.build_tag,
+            "start-datetime": config.start_datetime.isoformat(),
+            "end-datetime": config.end_datetime.isoformat(),
+        }
+
+        self._io = TransferClassifierIO(s3_manager, output_metadata)
+
+    def _read_spine_messages(self) -> Iterator[Message]:
+        input_paths = self._uris.spine_messages(self._reporting_window)
+        return self._io.read_spine_messages(input_paths)
+
+    def _write_transfers(
+        self, transfers: Iterator[Transfer], daily_start_datetime: datetime, cutoff: timedelta
+    ):
+        output_path = self._uris.gp2gp_transfers(
+            daily_start_datetime=daily_start_datetime, cutoff=cutoff
+        )
+        self._io.write_transfers(transfers, output_path)
+
+    def run(self):
+        transfer_observability_probe = TransferObservabilityProbe(logger=module_logger)
+        spine_messages = self._read_spine_messages()
+
+        transfer_service = TransferService(
+            message_stream=spine_messages,
+            cutoff=self._cutoff,
+            observability_probe=transfer_observability_probe,
+        )
+
+        conversations = transfer_service.group_into_conversations()
+        gp2gp_conversations = transfer_service.parse_conversations_into_gp2gp_conversations(
+            conversations
+        )
+
+        for daily_start_datetime in self._reporting_window.get_dates():
+            conversations_started_in_reporting_window = filter_conversations_by_day(
+                gp2gp_conversations, daily_start_datetime
+            )
+            transfers = transfer_service.convert_to_transfers(
+                conversations_started_in_reporting_window
+            )
+            self._write_transfers(
+                transfers=transfers, daily_start_datetime=daily_start_datetime, cutoff=self._cutoff
+            )
