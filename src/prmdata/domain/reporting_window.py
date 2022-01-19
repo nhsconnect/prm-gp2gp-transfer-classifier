a4e98bde77bcd1ae457b46465f039d5503befa36
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-01-19 14:47:46 +0000

    PRMT-2486 [HZ/SP] Refactor reporting window to remove redundant code

--- src/prmdata/domain/reporting_window.py
+++ src/prmdata/domain/reporting_window.py
@@ -16,18 +16,14 @@ class ReportingWindow:
         self._start_datetime = start_datetime
         self._end_datetime = end_datetime
 
-        self._validate_datetimes(start_datetime, end_datetime)
+        self._validate_datetimes(self._start_datetime, self._end_datetime)
 
+        yesterday_midnight_datetime = self._calculate_yesterday_midnight_datetime()
         if self._start_datetime is None:
-            self._start_datetime = (
-                self._calculate_yesterday_midnight_datetime() - conversation_cutoff
-            )
-
+            self._start_datetime = yesterday_midnight_datetime - conversation_cutoff
         if self._end_datetime is None:
             self._end_datetime = (
-                self._calculate_yesterday_midnight_datetime()
-                - conversation_cutoff
-                + timedelta(days=1)
+                yesterday_midnight_datetime - conversation_cutoff + timedelta(days=1)
             )
 
         self._dates = convert_date_range_to_dates(self._start_datetime, self._end_datetime)
@@ -42,13 +38,12 @@ class ReportingWindow:
             raise ValueError("Start datetime must be provided if end datetime is provided")
         if end_datetime is None and start_datetime:
             raise ValueError("End datetime must be provided if start datetime is provided")
-        if start_datetime:
-            self._validate_datetime_is_at_midnight(start_datetime)
-        if end_datetime:
-            self._validate_datetime_is_at_midnight(end_datetime)
+
+        self._validate_datetime_is_at_midnight(start_datetime)
+        self._validate_datetime_is_at_midnight(end_datetime)
 
     @staticmethod
-    def _validate_datetime_is_at_midnight(a_datetime: datetime):
+    def _validate_datetime_is_at_midnight(a_datetime: Optional[datetime]):
         midnight = time(hour=0, minute=0, second=0)
         if a_datetime and a_datetime.time() != midnight:
             raise ValueError("Datetime must be at midnight")
