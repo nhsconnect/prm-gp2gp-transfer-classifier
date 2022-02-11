c5bcea89fd128677611941ea55af51605cbe24ef
helen.zhou <helen.zhou1@nhs.net>
2022-02-11 11:54:10 +0000

    PRMT-TECH [SP/HZ] Add try catch and logging when file not found when attempting to read any files from S3

--- src/prmdata/utils/input_output/s3.py
+++ src/prmdata/utils/input_output/s3.py
@@ -27,11 +27,20 @@ class S3DataManager:
             extra={"event": "READING_FILE_FROM_S3", "object_uri": object_uri},
         )
         s3_object = self._object_from_uri(object_uri)
-        response = s3_object.get()
-        body = response["Body"]
-        with gzip.open(body, mode="rt") as f:
-            input_csv = csv.DictReader(f)
-            yield from input_csv
+
+        try:
+            response = s3_object.get()
+            body = response["Body"]
+            with gzip.open(body, mode="rt") as f:
+                input_csv = csv.DictReader(f)
+                yield from input_csv
+
+        except self._client.meta.client.exceptions.NoSuchKey:
+            logger.error(
+                f"File not found: {object_uri}, exiting...",
+                extra={"event": "FILE_NOT_FOUND_IN_S3", "object_uri": object_uri},
+            )
+            sys.exit(1)
 
     def read_json(self, object_uri: str):
         logger.info(
@@ -45,7 +54,7 @@ class S3DataManager:
         except self._client.meta.client.exceptions.NoSuchKey:
             logger.error(
                 f"File not found: {object_uri}, exiting...",
-                extra={"event": "FILE_NOT_FOUND_IN_S3"},
+                extra={"event": "FILE_NOT_FOUND_IN_S3", "object_uri": object_uri},
             )
             sys.exit(1)
 
