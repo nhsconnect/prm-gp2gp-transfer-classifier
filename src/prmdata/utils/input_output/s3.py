ebbf692448512f48528a2b8cb029b5ba22bf7ecb
helen.zhou <helen.zhou1@nhs.net>
2022-02-11 11:51:27 +0000

    PRMT-2542 [SP/HZ] Add read_json to S3 Manager

--- src/prmdata/utils/input_output/s3.py
+++ src/prmdata/utils/input_output/s3.py
@@ -1,6 +1,8 @@
 import csv
 import gzip
+import json
 import logging
+import sys
 from io import BytesIO
 from urllib.parse import urlparse
 
@@ -30,6 +32,25 @@ class S3DataManager:
         with gzip.open(body, mode="rt") as f:
             input_csv = csv.DictReader(f)
             yield from input_csv
+
+    def read_json(self, object_uri: str):
+        logger.info(
+            "Reading file from: " + object_uri,
+            extra={"event": "READING_FILE_FROM_S3", "object_uri": object_uri},
+        )
+        s3_object = self._object_from_uri(object_uri)
+
+        try:
+            response = s3_object.get()
+        except self._client.meta.client.exceptions.NoSuchKey:
+            logger.error(
+                f"File not found: {object_uri}, exiting...",
+                extra={"event": "FILE_NOT_FOUND_IN_S3"},
+            )
+            sys.exit(1)
+
+        body = response["Body"].read()
+        return json.loads(body.decode("utf8"))
 
     def write_parquet(self, table: Table, object_uri: str, metadata: dict[str, str]):
         logger.info(
---
+++ tests/unit/utils/io/s3/test_read_json_object.py
@@ -0,0 +1,41 @@
+from unittest import mock
+
+import boto3
+from moto import mock_s3
+
+from prmdata.utils.input_output.s3 import S3DataManager, logger
+from tests.unit.utils.io.s3 import MOTO_MOCK_REGION
+
+
+@mock_s3
+def test_read_json_object_returns_dictionary():
+    conn = boto3.resource("s3", region_name=MOTO_MOCK_REGION)
+    bucket = conn.create_bucket(Bucket="test_bucket")
+    s3_object = bucket.Object("test_object.json")
+    s3_object.put(Body=b'{"fruit": "mango"}')
+
+    s3_manager = S3DataManager(conn)
+
+    expected = {"fruit": "mango"}
+
+    actual = s3_manager.read_json("s3://test_bucket/test_object.json")
+
+    assert actual == expected
+
+
+@mock_s3
+def test_will_log_reading_file_event():
+    conn = boto3.resource("s3", region_name=MOTO_MOCK_REGION)
+    bucket = conn.create_bucket(Bucket="test_bucket")
+    s3_object = bucket.Object("test_object.json")
+    s3_object.put(Body=b'{"fruit": "mango"}')
+
+    s3_manager = S3DataManager(conn)
+    object_uri = "s3://test_bucket/test_object.json"
+
+    with mock.patch.object(logger, "info") as mock_log_info:
+        s3_manager.read_json(object_uri)
+        mock_log_info.assert_called_once_with(
+            f"Reading file from: {object_uri}",
+            extra={"event": "READING_FILE_FROM_S3", "object_uri": object_uri},
+        )
