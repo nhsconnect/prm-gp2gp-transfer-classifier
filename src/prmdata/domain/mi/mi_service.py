22c2a2f912fdc4bf87389b0a3b74963a98e61f83
Jake Newman <jacob.newman1@nhs.net>
2022-06-07 12:24:31 +0100

    PRMT-2653 [JN] Add more fields when constructing mi messages

--- src/prmdata/domain/mi/mi_service.py
+++ src/prmdata/domain/mi/mi_service.py
@@ -6,12 +6,23 @@ from typing import List
 class MiMessage:
     conversation_id: str
     event_id: str
+    event_generated_datetime: str
+    reporting_system_supplier: str
+    reporting_practice_ods_code: str
+    transfer_event_datetime: str
 
 
 class MiService:
     @staticmethod
     def construct_mi_messages_from_mi_events(mi_events: List[dict]) -> List[MiMessage]:
         return [
-            MiMessage(conversation_id=event["conversationId"], event_id=event["eventId"])
+            MiMessage(
+                conversation_id=event["conversationId"],
+                event_id=event["eventId"],
+                event_generated_datetime=event["eventGeneratedDateTime"],
+                reporting_system_supplier=event["reportingSystemSupplier"],
+                reporting_practice_ods_code=event["reportingPracticeOdsCode"],
+                transfer_event_datetime=event["transferEventDateTime"],
+            )
             for event in mi_events
         ]
--- tests/unit/domain/mi/test_mi_service.py
+++ tests/unit/domain/mi/test_mi_service.py
@@ -1,12 +1,36 @@
 from prmdata.domain.mi.mi_service import MiMessage, MiService
+from tests.builders.common import a_datetime, a_string
 
 
-def test_construct_messages_from_mi_events():
+def test_construct_mi_messages_from_mi_events():
     a_conversation_id = "1111-1111-1111-1111"
     an_event_id = "1234-5678-8765-4321"
-    mi_events = [{"eventId": an_event_id, "conversationId": a_conversation_id}]
+    a_random_datetime = a_datetime()
+    a_supplier = a_string()
+    an_ods_code = a_string()
+    another_datetime = a_datetime()
 
-    expected = [MiMessage(conversation_id=a_conversation_id, event_id=an_event_id)]
+    mi_events = [
+        {
+            "eventId": an_event_id,
+            "conversationId": a_conversation_id,
+            "eventGeneratedDateTime": a_random_datetime,
+            "reportingSystemSupplier": a_supplier,
+            "reportingPracticeOdsCode": an_ods_code,
+            "transferEventDateTime": another_datetime,
+        }
+    ]
+
+    expected = [
+        MiMessage(
+            conversation_id=a_conversation_id,
+            event_id=an_event_id,
+            event_generated_datetime=a_random_datetime,
+            reporting_system_supplier=a_supplier,
+            reporting_practice_ods_code=an_ods_code,
+            transfer_event_datetime=another_datetime,
+        )
+    ]
 
     actual = MiService.construct_mi_messages_from_mi_events(mi_events)
 
