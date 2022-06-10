269d4ea475185a202d2bd2ca28ddb8ab946be51f
Jake Newman <jacob.newman1@nhs.net>
2022-06-10 14:30:22 +0100

    PRMT-2653 [JN] Add mi message payload builders

--- tests/builders/mi_message.py
+++ tests/builders/mi_message.py
@@ -1,4 +1,9 @@
-from prmdata.domain.mi.mi_service import MiMessage
+from prmdata.domain.mi.mi_service import (
+    MiMessage,
+    MiMessagePayload,
+    MiMessagePayloadIntegration,
+    MiMessagePayloadRegistration,
+)
 from tests.builders.common import a_datetime, a_string
 
 
@@ -12,5 +17,28 @@ def build_mi_message(**kwargs) -> MiMessage:
         reporting_system_supplier=kwargs.get("reporting_system_supplier", a_string(8)),
         reporting_practice_ods_code=kwargs.get("reporting_practice_ods_code", a_string(8)),
         transfer_event_datetime=kwargs.get("transfer_event_datetime", a_datetime()),
-        payload=kwargs.get("payload", None),
+        payload=kwargs.get("payload", build_mi_message_payload(**kwargs)),
+    )
+
+
+def build_mi_message_payload(**kwargs) -> MiMessagePayload:
+    return MiMessagePayload(
+        registration=kwargs.get("registration", build_mi_message_payload_registration(**kwargs)),
+        integration=kwargs.get("integration", build_mi_message_payload_integration(**kwargs)),
+    )
+
+
+def build_mi_message_payload_registration(**kwargs) -> MiMessagePayloadRegistration:
+    return MiMessagePayloadRegistration(
+        registrationStartedDateTime=kwargs.get("registrationStartedDateTime", a_datetime()),
+        registrationType=kwargs.get("registrationType", a_string()),
+        requestingPracticeOdsCode=kwargs.get("requestingPracticeOdsCode", a_string()),
+        sendingPracticeOdsCode=kwargs.get("sendingPracticeOdsCode", a_string()),
+    )
+
+
+def build_mi_message_payload_integration(**kwargs) -> MiMessagePayloadIntegration:
+    return MiMessagePayloadIntegration(
+        integrationStatus=kwargs.get("integrationStatus", a_string()),
+        reason=kwargs.get("reason", a_string()),
     )
